bea8a47aa8d7fce1e1e7b568da3a7e22efb9a940
ally <allisterke@gmail.com>
2017-05-08 11:11:18 +0800

    deploy modified

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-ssh cuda 'rm -rf /var/www/ndbc2017/*'
-cat ndbc2017.tgz | ssh cuda 'tar xzf - -C /var/www/ndbc2017'
+if [ -z $1 ]; then
+	echo 'please specify which server to deploy'
+	exit 1
+fi
+
+bash pack.sh
+#ssh cuda 'rm -rf /var/www/ndbc2017/*'
+cat ndbc2017.tgz | ssh $1 'tar xzf - -C /var/www/ndbc2017'
