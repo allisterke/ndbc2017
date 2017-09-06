7bb17710603ce52fd2e245aa371aaffcc545e371
ally <allisterke@gmail.com>
2017-09-06 14:55:24 +0800

    add invitation

--- pack.sh
+++ pack.sh
@@ -4,4 +4,4 @@ FILE=ndbc2017.tgz
 
 rm -f $FILE
 
-tar czf $FILE images *.js *.css $(ls *.html | grep -v '\.meta\.' | grep -v template)
+tar czf $FILE images *.js *.css *.pdf $(ls *.html | grep -v '\.meta\.' | grep -v template)
